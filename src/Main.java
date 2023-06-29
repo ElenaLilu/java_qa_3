29a058357f4b45e08bc30e5db10f5c3b23c06476
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-06-29 09:58:19 +0300

    ssrc/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
+        int price = 25_000;
+        int miles = service.calculate(price); // должно получиться 1250
         System.out.println(miles);
     }
 }
